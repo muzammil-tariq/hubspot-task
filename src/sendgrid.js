e58b0fc77ae84a84a49125151ecf4330e11a763d
muzammil-tariq <muzammil413tariq@gmail.com>
2025-03-07 01:49:34 +0500

    fix param name

--- src/sendgrid.js
+++ src/sendgrid.js
@@ -3,13 +3,13 @@ const logger = require('./logger');
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-async function sendNotification({ total, createdCount, createdCount }) {
+async function sendNotification({ total, updatedCount, createdCount }) {
   try {
     const msg = {
       to: process.env.ADMIN_EMAIL,
       from: process.env.SENDGRID_FROM_EMAIL,
       subject: 'HubSpot Automation Report',
-      text: `Total: ${total}, Updated Deals: ${createdCount}, New Deals: ${createdCount}`,
+      text: `Total: ${total}, Updated Deals: ${updatedCount}, New Deals: ${createdCount}`,
     };
 
     await sgMail.send(msg);
